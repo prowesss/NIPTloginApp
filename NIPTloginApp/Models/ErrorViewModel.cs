4b049e9be0a1a034aab0a18f6ed80b65116a7e1a
umesheo <52323325+umesheo@users.noreply.github.com>
2023-04-13 20:39:36 +1200

    Update ErrorViewModel.cs

--- NIPTloginApp/Models/ErrorViewModel.cs
+++ NIPTloginApp/Models/ErrorViewModel.cs
@@ -2,8 +2,8 @@ namespace NIPTloginApp.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string? requestid { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool showrequestid => !string.IsNullOrEmpty(requestid);
     }
 }
