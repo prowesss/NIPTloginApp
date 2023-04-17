c23ac81d312639bac1e2b1c23e9357b8f860bc03
prowesss <39946326+prowesss@users.noreply.github.com>
2023-04-17 13:18:01 +1200

    Update HomeController.cs

--- NIPTloginApp/Controllers/HomeController.cs
+++ NIPTloginApp/Controllers/HomeController.cs
@@ -2,8 +2,10 @@
 using NIPTloginApp.Models;
 using System.Diagnostics;
 
+// Reviewer Prowess: wrong namespace standard
 namespace niptloginapp.controllers
 {
+// Reviewer Prowess: change class to correct naming convention as homeController
     public class homecontroller : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -12,7 +14,7 @@ namespace niptloginapp.controllers
         {
             _logger = logger;
         }
-
+// Reviewer Prowess: change interface name 
         public ActionResult Index()
         {
             return View();
@@ -25,6 +27,8 @@ namespace niptloginapp.controllers
        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        
+        // Reviewer Prowess: wrong method name here
         public iactionresult error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
